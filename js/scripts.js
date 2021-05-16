9c51ec0b508ad2b61fa23e8d6816c35e4af7a3b5
bob <vova_progmush@mail.ru>
2021-05-16 18:52:17 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+  
+     //>=, not <=
+    if (scroll > 700) {
+        //clearHeader, not clearheader - caps H
+        $(".navbar").addClass("navbar_scrolled");
+    } else {
+      $(".navbar").removeClass("navbar_scrolled");
+    }
+  });  
+
+
+$(".progress-bar").loading();
+
